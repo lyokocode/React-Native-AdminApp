3c66cb2f49005ea485145b3870cf78f9188212fd
lyokocode <mustafausakli_@hotmail.com>
2023-02-17 21:20:21 +0300

    create react native and NativeWind

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-blue-500">
+      <Text className="text-black">Hello World</Text>
+      <StatusBar style="auto" />
+
+    </View>
+  );
+}
